63d6e8565e25209ccc14b1207cbe992647d34c96
itzAle <alexcs9230@gmail.com>
2024-09-03 23:40:53 +0200

    fix children error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import type { ReactNode } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
@@ -8,7 +9,7 @@ export const metadata = {
   description: "An extensive API for managing video games",
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" className="dark">
       <body className="bg-gray-900 text-white">{children}</body>
