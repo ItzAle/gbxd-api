0c8509d5a6a25e1ee07add163b69b315942baef1
ale <alexcs9230@gmail.com>
2024-09-17 19:26:20 +0200

    revoke

--- app/api/add-game/route.js
+++ app/api/add-game/route.js
@@ -1,10 +1,36 @@
-import { NextResponse } from "next/server";
-import { PutCommand } from "@aws-sdk/lib-dynamodb";
 import { docClient } from "../../../lib/aws-config";
+import { PutCommand } from "@aws-sdk/lib-dynamodb";
 import slugify from "slugify";
+import { NextResponse } from "next/server";
+import { LRUCache } from "lru-cache";
+
+// Configuración del rate limiting
+const rateLimit = new LRUCache({
+  max: 500,
+  ttl: 60 * 1000, // 1 minuto
+});
+
+const RATE_LIMIT = 5; // Número máximo de solicitudes por minuto
+
+function rateLimiter(ip) {
+  const tokenCount = rateLimit.get(ip) || 0;
+  if (tokenCount > RATE_LIMIT) {
+    return false;
+  }
+  rateLimit.set(ip, tokenCount + 1);
+  return true;
+}
 
 // Handler para el POST request para añadir un juego
 export async function POST(req) {
+  // Obtener la IP del cliente
+  const ip = req.headers.get("x-forwarded-for") || req.ip;
+
+  // Verificar el rate limit
+  if (!rateLimiter(ip)) {
+    return NextResponse.json({ error: "Rate limit exceeded" }, { status: 429 });
+  }
+
   try {
     // Parsear el cuerpo de la solicitud
     const {
@@ -21,7 +47,6 @@ export async function POST(req) {
       storeLinks,
       aliases,
       franchises,
-      isTBA,
     } = await req.json();
 
     // Validar que todos los campos necesarios estén presentes
@@ -35,7 +60,10 @@ export async function POST(req) {
       !genres ||
       !coverImageUrl
     ) {
-      return NextResponse.json({ error: "All fields are required" }, { status: 400 });
+      return NextResponse.json(
+        { error: "All fields are required" },
+        { status: 400 }
+      );
     }
 
     // Generar un slug para el juego
@@ -47,7 +75,7 @@ export async function POST(req) {
       Item: {
         slug,
         name,
-        releaseDate: isTBA ? "TBA" : releaseDate,
+        releaseDate,
         description,
         publisher,
         developer,
@@ -59,14 +87,16 @@ export async function POST(req) {
         storeLinks,
         aliases,
         franchises,
-        isTBA,
       },
     });
 
     await docClient.send(command);
 
     // Responder con un mensaje de éxito
-    return NextResponse.json({ message: "Game added successfully" }, { status: 200 });
+    return NextResponse.json(
+      { message: "Game added successfully" },
+      { status: 200 }
+    );
   } catch (error) {
     console.error("Error adding game:", error);
     return NextResponse.json({ error: "Error adding game" }, { status: 500 });
