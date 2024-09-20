8e6f533a01adbfd1d4d185004da569b9343bcf64
ale <alexcs9230@gmail.com>
2024-09-20 22:58:53 +0200

    update page

--- app/donate/page.tsx
+++ app/donate/page.tsx
@@ -1,9 +1,10 @@
 "use client";
 
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Typography, Container, Box, Button } from "@mui/material";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
-
+import { Heart } from "lucide-react";
+import Confetti from "react-confetti";
 import Navbar from "../components/Navbar";
 
 const theme = createTheme({
@@ -24,18 +25,50 @@ const theme = createTheme({
     fontFamily: "'Poppins', sans-serif",
   },
 });
+
 export default function DonatePage() {
+  const [windowDimension, setWindowDimension] = useState({
+    width: 0,
+    height: 0,
+  });
+  const [showConfetti, setShowConfetti] = useState(true);
+
+  useEffect(() => {
+    setWindowDimension({
+      width: window.innerWidth,
+      height: window.innerHeight,
+    });
+  }, []);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setShowConfetti(false);
+    }, 5000); // Stop confetti after 5 seconds
+
+    return () => clearTimeout(timeout);
+  }, []);
+
   return (
     <ThemeProvider theme={theme}>
+      {showConfetti && (
+        <Confetti
+          width={windowDimension.width}
+          height={windowDimension.height}
+          recycle={false}
+          numberOfPieces={200}
+          gravity={0.1}
+        />
+      )}
       <Navbar />
       <Container maxWidth="md">
         <Box sx={{ my: 4, textAlign: "center" }}>
           <Typography variant="h2" component="h1" gutterBottom>
-            Thank you for your donation
+            Thank you for your donation!
           </Typography>
           <Typography variant="h5" component="p" gutterBottom>
             Your support helps us maintain the servers and improve the API
           </Typography>
+
           <Button
             variant="contained"
             color="primary"
@@ -44,6 +77,7 @@ export default function DonatePage() {
             target="_blank"
             rel="noopener noreferrer"
             sx={{ mt: 2 }}
+            startIcon={<Heart />}
           >
             Donate with PayPal
           </Button>
